2bf9ccd82a7adafb3586f9b0bd2bf6e6aaedb5c3
Elaidzha1940 <eli080293@gmail.com>
2023-10-24 23:59:54 +0300

    ParallaxEffect

---
+++ ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by Elaidzha Shchukin on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
