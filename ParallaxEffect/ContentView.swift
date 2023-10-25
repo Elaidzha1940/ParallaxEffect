de1a982eeadde1c1ffd42731bb9e6aace239da4b
Elaidzha1940 <eli080293@gmail.com>
2023-10-25 03:23:51 +0300

    Add ParallaxEffect

--- ParallaxEffect/ContentView.swift
+++ ParallaxEffect/ContentView.swift
@@ -13,33 +13,52 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-         
+            ScrollView {
+                ContentList()
+                ParallaxEffect()
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
 
+struct ParallaxEffect: View {
+    var body: some View {
+        GeometryReader(content: { geometry in
+           Spacer()
+            
+            // background
+                .background {
+                    Image("ai")
+                        .resizable()
+                        .scaledToFit()
+                }
+        })
+    }
+}
+ // MARK: Content List
 struct ContentList: View {
     var body: some View {
         
         VStack(alignment: .leading) {
             ForEach(0..<10) { _ in
-                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                RoundedRectangle(cornerRadius: 15, style: .circular)
                     .frame(width: 120, height: 30)
                 VStack(alignment: .leading) {
                     RoundedRectangle(cornerRadius: 15, style: .continuous)
-                        .frame(width: 250, height: 25)
+                        .frame(width: 280, height: 25)
 
                     RoundedRectangle(cornerRadius: 15, style: .continuous)
-                        .frame(width: 250, height: 25)
+                        .frame(width: 210, height: 25)
                     
                     RoundedRectangle(cornerRadius: 15, style: .continuous)
                         .frame(width: 150, height: 25)
                 }
-                .opacity(0.4)
+                .opacity(0.2)
             }
             .padding(.leading)
         }
